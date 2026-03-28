efef682614904cd8e48bc93b728260feb31f1dbe
certifiablyaplomb <craftygaming1063@gmail.com>
2026-03-28 01:47:33 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const hamburgerIcon = document.querySelector('.js-hamburger-icon');
+const hamburgerMenu = document.querySelector('.js-hamburger-header');
+const hamburgerMenuButtons = document.querySelector('.js-hamburger-menu-buttons-list');
+let clicked = false;
+hamburgerIcon.addEventListener("click", () => {
+    clicked = !clicked;
+    if (clicked){
+        hamburgerIcon.style.transform="rotate(90deg)";
+        hamburgerMenu.style.backgroundColor = "rgba(255, 244, 229, .1)";
+
+        hamburgerMenuButtons.classList.toggle('active');
+        hamburgerMenuButtons.style.opacity= "1";
+        hamburgerMenuButtons.style.visibility= "visible";
+    }
+    else{
+        hamburgerIcon.style.transform = clicked ? "rotate(90deg)" : "rotate(0deg)";
+        hamburgerMenu.style.backgroundColor = "transparent";
+
+        hamburgerMenuButtons.classList.toggle('active');
+        hamburgerMenuButtons.style.opacity= "0";
+        hamburgerMenuButtons.style.visibility= "hidden";
+
+    }
+})
+
