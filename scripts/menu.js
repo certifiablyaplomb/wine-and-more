b0fb42f7c5b2c6c4b08585c2bb70afced8e329fa
certifiablyaplomb <craftygaming1063@gmail.com>
2026-03-28 21:16:37 -0700

    finsihed menu mvp-- top toggle works :)

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,9 +1,18 @@
 const arrowButtons = document.querySelectorAll(".js-right-arrow");
-
+const description = document.querySelector(".js-description");
 
 arrowButtons.forEach((button)=>{
     button.addEventListener('click', ()=>{
-        let rotated = button.style.transform === "rotate(90deg)"
-        button.style.transform = rotated ? "rotate(0deg)" : "rotate(90deg)";
+        let rotated = button.style.transform === "rotate(90deg)";
+        if (rotated){
+            button.style.transform = "rotate(0deg)";
+            description.style.maxHeight = "0px"
+            description.style.opacity = "0"
+        }
+        else{
+            button.style.transform = "rotate(90deg)";
+            description.style.maxHeight = "300px"
+            description.style.opacity = "1"
+        }
     })
 })
