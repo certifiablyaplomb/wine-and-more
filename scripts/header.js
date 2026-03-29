455eb5705c9130dd2fa1209fba15d2cb268989a3
certifiablyaplomb <craftygaming1063@gmail.com>
2026-03-29 02:15:07 -0700

    final push for MVP

--- scripts/header.js
+++ scripts/header.js
@@ -1,12 +1,25 @@
 const hamburgerIcon = document.querySelector('.js-hamburger-icon');
 const hamburgerMenu = document.querySelector('.js-hamburger-header');
 const hamburgerMenuButtons = document.querySelector('.js-hamburger-menu-buttons-list');
+
+const current_path= window.location.pathname;
+
 let clicked = false;
+if (current_path != '/index.html'){
+    clicked = true;
+    hamburgerIcon.style.transform="rotate(90deg)";
+    hamburgerMenu.style.backgroundColor = "rgba(119, 119, 119, 0.9)";
+
+    hamburgerMenuButtons.classList.toggle('active');
+    hamburgerMenuButtons.style.opacity= "1";
+    hamburgerMenuButtons.style.visibility= "visible";
+}
+
 hamburgerIcon.addEventListener("click", () => {
     clicked = !clicked;
     if (clicked){
         hamburgerIcon.style.transform="rotate(90deg)";
-        hamburgerMenu.style.backgroundColor = "rgba(216, 216, 216, .9)";
+        hamburgerMenu.style.backgroundColor = "rgba(119, 119, 119, 0.9)";
 
         hamburgerMenuButtons.classList.toggle('active');
         hamburgerMenuButtons.style.opacity= "1";
