27425ee5ec14897073ecbb928c3edc139e72c30a
certifiablyaplomb <craftygaming1063@gmail.com>
2026-04-08 22:32:04 -0700

    commit before i go crazy with something really weird on the borders of the menu

---
+++ data/menu_info.js
@@ -0,0 +1,89 @@
+export const wWines = [
+    {
+        name:"2018 Rose' of Petite Sirah - Rio Claro Wines",
+        description: "This beautiful rosé from Rio Claro showcases Petite Sirah in an unexpected and elegant form — deep salmon-pink in the glass with aromas of fresh strawberry, watermelon, and a subtle hint of black pepper that nods to its bold varietal roots.",
+        glassPrice: 12,
+        bottlePrice: 36,
+        id:"ww0"
+    },
+    {
+        name:"2023 Trullo Rheinbessen Riesling",
+        description: "none",
+        glassPrice: 12,
+        bottlePrice: 25,
+        id:"ww1"
+    },
+    {
+        name:"2023 White Blen - Stone Rose - Napa",
+        description: "none",
+        glassPrice: 14,
+        bottlePrice: 38,
+        id:"ww2"
+    },
+    {
+        name:"2023 Gran Blanco - Rio Claro Wines",
+        description: "none",
+        glassPrice: 12,
+        bottlePrice: 30,
+        id:"ww3"
+    },
+    {
+        name:'2022 Vurna Bogona - "Sto Fuori" Bianca',
+        description: "none",
+        glassPrice: 12,
+        bottlePrice: 30,
+        id:"ww4"
+    },
+    {
+        name:"2023 Chardonnay - Ramsey - California",
+        description: "none",
+        glassPrice: 12,
+        bottlePrice: 35,
+        id:"ww5"
+    },
+]
+
+export const rWines = [
+    {
+        name:"2021 Red Blend - Stone Rose - Napa",
+        description: "none",
+        glassPrice: 12,
+        bottlePrice: 28,
+        id:"rw0"
+    },
+    {
+        name:"2021 Butler Hill - Pinot Noit - OR",
+        description: "none",
+        glassPrice: 15,
+        bottlePrice: 38,
+        id:"rw1"
+    },
+    {
+        name:"2022 Clssico Chianti - Balisca Cafaggio Estate",
+        description: "none",
+        glassPrice: 15,
+        bottlePrice: 42,
+        id:"rw2"
+    },
+    {
+        name:"2021 Primos - Rio Claro Winery",
+        description: "none",
+        glassPrice: 15,
+        bottlePrice: 42,
+        id:"rw3"
+    },
+    {
+        name:'2021 Cabernet Franc - Rio Claro Winery',
+        description: "none",
+        glassPrice: 15,
+        bottlePrice: 42,
+        id:"rw4"
+    },
+    {
+        name:"2023 Malbec - Sur De Los Andes Classico - Argentina",
+        description: "none",
+        glassPrice: 12,
+        bottlePrice: 38,
+        id:"rw5"
+    },
+]
--- scripts/menu.js
+++ scripts/menu.js
@@ -1,7 +1,54 @@
+import {wWines, rWines} from "../data/menu_info.js";
+
+
+
+const wWinesSec = document.querySelector(".js-white-wines");
+const rWinesSec = document.querySelector(".js-red-wines");
+
+
+
+//turn into function that returns html, takes menu name as parameter, and list variable menu-info
+function buildWMenuSec(sectionName, menuList){
+    let html = `<div class="title">${sectionName}</div>
+          <div class="item-line">
+            <div class="item"></div> 
+            <div class="type">Glass</div> 
+            <div class="type">Bottle</div> 
+          </div>`
+    menuList.forEach((wine)=>{
+        html+=`
+        <div class="item-line">
+            <div class="item">
+                ${wine.name}
+                <div class="read-more-section">
+                <div class="read-more">
+                    read more 
+                    <img class="right-arrow js-right-arrow" id="${wine.id}" src="images/circle-arrow-right-w.svg">
+                </div>
+                <div class="description js-description-${wine.id}">
+                    ${wine.description}
+                </div>           
+                </div>
+            </div> 
+            <div class="price glassprice">$${wine.glassPrice}</div> 
+            <div class="price bottleprice">$${wine.bottlePrice}</div> 
+        </div>
+        `
+    })
+    return html;
+}
+
+wWinesSec.innerHTML += buildWMenuSec("White Wines", wWines)
+rWinesSec.innerHTML += buildWMenuSec("Red Wines", rWines)
+
+
+
 const arrowButtons = document.querySelectorAll(".js-right-arrow");
-const description = document.querySelector(".js-description");
 
 arrowButtons.forEach((button)=>{
+    const id = button.getAttribute("id");
+    console.log(id)
+    const description = document.querySelector(`.js-description-${id}`);
     button.addEventListener('click', ()=>{
         let rotated = button.style.transform === "rotate(90deg)";
         if (rotated){
