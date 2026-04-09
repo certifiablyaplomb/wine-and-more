e72cb92fdc21a2d82bfb85706963f5ec16aba34f
certifiablyaplomb <craftygaming1063@gmail.com>
2026-04-09 03:05:02 -0700

    i think im done for the night

---
+++ data/reviews.js
@@ -0,0 +1,17 @@
+export const reviews = [
+    {
+        stars: 5,
+        review: "Loved the selection of wines and the flight options.",
+        author: "Lindsay M"
+    },
+    {
+        stars: 5,
+        review: 'This has a very " Speak easy feel"',
+        author: "Elizabeth Husak"
+    },
+    {
+        stars: 5,
+        review: "Amazing service, wonderful atmosphere, friendly staff.",
+        author: "Gabby Sandy"
+    },
+]
--- scripts/about.js
+++ scripts/about.js
@@ -6,13 +6,13 @@ const leftImage = document.querySelector('.js-display-image-right');
 const mainImage = document.querySelector('.js-display-image-main');
 const rightImage = document.querySelector('.js-display-image-right');
 
-let currentIndex= 0;
+let currentImageIndex= 0;
 
 loadImages();
 function loadImages(){
-    let leftImageSrc = imageList[convert(currentIndex - 1)].source;
-    let mainImageSrc = imageList[currentIndex].source;
-    let rightImageSrc = imageList[convert(currentIndex + 1)].source;
+    let leftImageSrc = imageList[convert(currentImageIndex - 1, imageList)].source;
+    let mainImageSrc = imageList[currentImageIndex].source;
+    let rightImageSrc = imageList[convert(currentImageIndex + 1, imageList)].source;
     leftImage.src = `${leftImageSrc}`
     mainImage.src = `${mainImageSrc}`
     rightImage.src = `${rightImageSrc}`
@@ -22,19 +22,64 @@ function loadImages(){
 
 
 leftArrow.addEventListener('click', ()=>{
-    currentIndex-=1;
-    currentIndex = convert(currentIndex)
+    currentImageIndex-=1;
+    currentImageIndex = convert(currentImageIndex, imageList)
     loadImages();
 })
 
 rightArrow.addEventListener('click', ()=>{
-    currentIndex +=1;
-    currentIndex = convert(currentIndex)
+    currentImageIndex +=1;
+    currentImageIndex = convert(currentImageIndex, imageList)
     loadImages();
 })
 
-function convert(index){
-    if(index < 0) index = imageList.length - 1;
-    else if(index >= imageList.length) index = 0;
+function convert(index, listname){
+    if(index < 0) index = listname.length - 1;
+    else if(index >= listname.length) index = 0;
     return index;
 }
+
+
+import { reviews } from "../data/reviews.js";
+const reviewSection = document.querySelector('.js-review-section');
+
+let currentReviewIndex = 0;
+
+function updateReviewHTML(){
+    reviewSection.innerHTML = `
+        <h2 class="reviews-title">What Our Customers Say</h2>
+        <div class="review-container">
+            <span class="arrow left-arrow js-left-review-arrow">&#10094;</span>
+            <div class="review-box">
+            <p class="review-text">“${reviews[currentReviewIndex].review}”</p>
+            <span class="review-author">– ${reviews[currentReviewIndex].author}</span>
+            </div>
+            <span class="arrow right-arrow js-right-review-arrow">&#10095;</span>
+        </div>
+       <p class="read-more">
+        <a href="https://www.google.com/maps/place/Wine+Tasting+and+More-+Munds+Park/@34.9405053,-111.656227,17z/data=!4m16!1m7!3m6!1s0x872dbd002e239263:0x74393081772c990a!2sWine+Tasting+and+More-+Munds+Park!8m2!3d34.9405009!4d-111.6536521!16s%2Fg%2F11xz6dvk96!3m7!1s0x872dbd002e239263:0x74393081772c990a!8m2!3d34.9405009!4d-111.6536521!9m1!1b1!16s%2Fg%2F11xz6dvk96?entry=ttu&g_ep=EgoyMDI2MDQwNi4wIKXMDSoASAFQAw%3D%3D" 
+            target="_blank" 
+            rel="noopener">
+            Read more on Google
+        </a>
+    </p>`
+;
+    const leftReviewArrow = document.querySelector('.js-left-review-arrow');
+    const rightReviewArrow = document.querySelector('.js-right-review-arrow');
+
+    leftReviewArrow.addEventListener('click', ()=>{
+        currentReviewIndex-=1;
+        currentReviewIndex = convert(currentReviewIndex, reviews)
+        updateReviewHTML();
+    })
+    rightReviewArrow.addEventListener('click', ()=>{
+        currentReviewIndex+=1;
+        currentReviewIndex = convert(currentReviewIndex, reviews)
+        updateReviewHTML();
+    })
+
+}
+updateReviewHTML();
+
+
+
